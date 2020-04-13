b3436e020a449ebda316694a8f1c6d23f52c7b0c
merinovroman <merinovroman@gmail.com>
2020-04-13 23:52:19 +0500

    добавил gitignore

--- src/data/www/bitrix/managed_cache/MYSQL/user_option/80/80d0df1c506cfa11d255d489ea901529.php
+++ src/data/www/bitrix/managed_cache/MYSQL/user_option/80/80d0df1c506cfa11d255d489ea901529.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001586803590';
-$dateexpire = '001586807190';
+$datecreate = '001586803903';
+$dateexpire = '001586807503';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";N;}';
 return true;
 ?>
