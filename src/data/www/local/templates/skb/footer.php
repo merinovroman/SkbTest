a0b2133f39c9fc90ceafc0756e121114410192e2
merinovroman <merinovroman@gmail.com>
2020-04-14 02:23:17 +0500

    мобильной адаптации хедера и футера

--- src/data/www/local/templates/skb/footer.php
+++ src/data/www/local/templates/skb/footer.php
@@ -21,7 +21,7 @@
             </div>
             <div class="row">
                 <div class="col-12 col-md-5">
-                    <div class="row">
+                    <div class="row block-footer-menu">
                         <div class="col-12 col-sm footer-menu">
                             <p><a href="#">О банке</a></p>
                             <p><a href="#">Новости</a></p>
@@ -38,53 +38,68 @@
                             <p><a href="#">Раскрытие информации</a></p>
                         </div>
                     </div>
+                    <div class="row block-footer-modile-menu text-left">
+                        <div class="col-6 col-sm footer-menu">
+                            <p><a href="#">О банке</a></p>
+                            <p><a href="#">Новости</a></p>
+                            <p><a href="#">Акции и бонусы</a></p>
+                            <p><a href="#">Партнерам</a></p>
+                            <p><a href="#">Вакансии</a></p>
+                        </div>
+                        <div class="col-6 col-sm footer-menu">
+                            <p><a href="#">Реквизиты</a></p>
+                            <p><a href="#">Пресс-центр</a></p>
+                            <p><a href="#">Офисы и банкоматы</a></p>
+                            <p><a href="#">Раскрытие информации</a></p>
+                        </div>
+                    </div>
                 </div>
-                <div class="offset-md-1 col-12 col-md-3">
+                <div class="offset-md-1 col-12 col-md-3 text-left">
                     <div class="row block-curs-valut">
-                        <div class="col-4 col-sm item-curs-valut">
+                        <div class="col-3 col-sm item-curs-valut">
                             <p></p>
                         </div>
-                        <div class="col-4 col-sm item-curs-valut">
+                        <div class="col-3 col-sm item-curs-valut">
                             <p>Покупка</p>
                         </div>
-                        <div class="col-4 col-sm item-curs-valut">
+                        <div class="col-3 col-sm item-curs-valut">
                             <p>Продажа</p>
                         </div>
-                        <div class="col-4 col-sm item-curs-valut">
+                        <div class="col-3 col-sm item-curs-valut">
                             <p>ЦБ РФ</p>
                         </div>
                     </div>
                     <div class="row block-curs-valut-value">
-                        <div class="col-4 col-sm">
+                        <div class="col-3 col-sm">
                             <p>USD</p>
                         </div>
-                        <div class="col-4 col-sm">
+                        <div class="col-3 col-sm">
                             <p>63,04</p>
                         </div>
-                        <div class="col-4 col-sm">
+                        <div class="col-3 col-sm">
                             <p>65,22</p>
                         </div>
-                        <div class="col-4 col-sm">
+                        <div class="col-3 col-sm">
                             <p>65,22</p>
                         </div>
                     </div>
                     <div class="row block-curs-valut-value">
-                        <div class="col-4 col-sm">
+                        <div class="col-3 col-sm">
                             <p>EUR</p>
                         </div>
-                        <div class="col-4 col-sm">
+                        <div class="col-3 col-sm">
                             <p>68,89</p>
                         </div>
-                        <div class="col-4 col-sm">
+                        <div class="col-3 col-sm">
                             <p>72,34</p>
                         </div>
-                        <div class="col-4 col-sm">
+                        <div class="col-3 col-sm">
                             <p>72,34</p>
                         </div>
                     </div>
 
                 </div>
-                <div class="offset-md-1 col-12 col-md-2">
+                <div class="offset-md-1 col-12 col-md-2 text-left">
                     <div class="row free-russia">
                         <div class="col-12 col-sm">
                             Бесплатно по России
@@ -106,7 +121,7 @@
     </footer>
     <footer class="bg-gradient">
         <div class="container">
-            <div class="row">
+            <div class="row text-left">
                 <div class="col-12 col-md-5 copyright">
                     © СКБ-банк, 2019 Генеральная лицензия № 705<br /> Центрального банка Российской Федерации
                 </div>
