211995b4c1765dc027e4652cee56653e9cf4445a
merinovroman <merinovroman@gmail.com>
2020-04-14 23:57:34 +0500

    раздел Кредиты Получить кредит

--- src/data/www/chastnym-licam/kredity/index.php
+++ src/data/www/chastnym-licam/kredity/index.php
@@ -1,143 +1,173 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Кредиты");
 ?>
 
-<div class="row card-deck">
-    <div class="col-12 col-sm">
-        <div class="block-main-top">
-            <div class="block-green"></div>
-            <div class="block-samokat"></div>
-            <div class="row">
-                <div class="col-8 col-md-8">
-                    <h3>Быстрый кредит за 5 минут</h3>
-                    <div class="row">
-                        <div class="col-12 col-sm">
-                            от <span class="big-text">9.9%</span><br />
-                            ставка на весь срок
+    <div class="row card-deck">
+        <div class="col-12 col-sm">
+            <div class="block-main-top">
+                <div class="block-green"></div>
+                <div class="block-samokat"></div>
+                <div class="row">
+                    <div class="col-8 col-md-8">
+                        <h3>Быстрый кредит за 5 минут</h3>
+                        <div class="row">
+                            <div class="col-12 col-sm">
+                                от <span class="big-text">9.9%</span><br/>
+                                ставка на весь срок
+                            </div>
+                            <div class="col-12 col-sm">
+                                до <span class="big-text">250</span> тыс. <span class="big-text">₽</span><br/>
+                                максимальная сумма
+                            </div>
+                            <div class="col-12 col-sm doki ">
+                                по <span class="big-text">1</span> документу<br/>
+                                нужен только паспорт
+                            </div>
                         </div>
-                        <div class="col-12 col-sm">
-                            до <span class="big-text">250</span> тыс. <span class="big-text">₽</span><br />
-                            максимальная сумма
-                        </div>
-                        <div class="col-12 col-sm doki ">
-                            по <span class="big-text">1</span> документу<br />
-                            нужен только паспорт
+                        <div class="row">
+                            <div class="col-12 col-sm block-btn">
+                                <a href="#" class="btn-read">Оформить кредит</a>
+                            </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-12 col-sm block-btn">
-                            <a href="#" class="btn-read">Оформить кредит</a>
-                        </div>
+                    <div class="col-12 col-md-6"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row cards-deck">
+        <div class="col-12 col-lg-6 block-item item-mch">
+            <div class="item-card">
+                <div class="block-img"></div>
+                <div class="row">
+                    <div class="col-6">
+                        <h4>Кредит для пpедпpинимателей</h4>
                     </div>
                 </div>
-                <div class="col-12 col-md-6"></div>
+                <div class="row">
+                    <div class="col-12 col-xl-4">
+                        до <span class="big-text">250</span> тыс. <span class="big-text">₽</span><br/>
+                        <span class="small-text">максимальная сумма</span>
+                    </div>
+                    <div class="col-12 col-xl-4">
+                        от <span class="big-text">9.9%</span><br/>
+                        <span class="small-text">ставка на весь срок</span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 more-dierails border-top">
+                        <a href="#">Подробнее</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-6 block-item item-mch">
+            <div class="item-card">
+                <div class="block-img"></div>
+                <div class="row">
+                    <div class="col-6">
+                        <h4>Кредит для пpедпpинимателей</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-xl-4">
+                        до <span class="big-text">250</span> тыс. <span class="big-text">₽</span><br/>
+                        <span class="small-text">максимальная сумма</span>
+                    </div>
+                    <div class="col-12 col-xl-4">
+                        от <span class="big-text">9.9%</span><br/>
+                        <span class="small-text">ставка на весь срок</span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 more-dierails border-top">
+                        <a href="#">Подробнее</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-6 block-item item-mch">
+            <div class="item-card">
+                <div class="block-img"></div>
+                <div class="row">
+                    <div class="col-6">
+                        <h4>Кредит для пpедпpинимателей</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-xl-4">
+                        до <span class="big-text">250</span> тыс. <span class="big-text">₽</span><br/>
+                        <span class="small-text">максимальная сумма</span>
+                    </div>
+                    <div class="col-12 col-xl-4">
+                        от <span class="big-text">9.9%</span><br/>
+                        <span class="small-text">ставка на весь срок</span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 more-dierails border-top">
+                        <a href="#">Подробнее</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-6 block-item item-mch">
+            <div class="item-card">
+                <div class="block-img"></div>
+                <div class="row">
+                    <div class="col-6">
+                        <h4>Кредит для пpедпpинимателей</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-xl-4">
+                        до <span class="big-text">250</span> тыс. <span class="big-text">₽</span><br/>
+                        <span class="small-text">максимальная сумма</span>
+                    </div>
+                    <div class="col-12 col-xl-4">
+                        от <span class="big-text">9.9%</span><br/>
+                        <span class="small-text">ставка на весь срок</span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 more-dierails border-top">
+                        <a href="#">Подробнее</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="row cards-deck">
-    <div class="col-12 col-lg-6 block-item item-mch">
-        <div class="item-card">
-            <div class="block-img"></div>
-            <div class="row">
-                <div class="col-6">
-                    <h4>Кредит для пpедпpинимателей</h4>
+    <div class="row cards-deck">
+        <div class="col-12">
+            <div class="kdNHTS">
+                <div class="Advantagesstyled__TitleContainer-sc-67zj0h-1 cboULR"><h3
+                            class="Advantagesstyled__Title-sc-67zj0h-3 fatiKL">Получить кредит <span
+                                class="Advantagesstyled__ColoredText-sc-67zj0h-4 eABJLy">просто!</span></h3>
+                    <p class="Advantagesstyled__Text-sc-67zj0h-5 boHdLj">Итоговое решение без&nbsp;визита в&nbsp;офис</p>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-xl-4">
-                    до <span class="big-text">250</span> тыс. <span class="big-text">₽</span><br />
-                    <span class="small-text">максимальная сумма</span>
-                </div>
-                <div class="col-12 col-xl-4">
-                    от <span class="big-text">9.9%</span><br />
-                    <span class="small-text">ставка на весь срок</span>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-6 more-dierails border-top">
-                    <a href="#">Подробнее</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-12 col-lg-6 block-item item-mch">
-        <div class="item-card">
-            <div class="block-img"></div>
-            <div class="row">
-                <div class="col-6">
-                    <h4>Кредит для пpедпpинимателей</h4>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-xl-4">
-                    до <span class="big-text">250</span> тыс. <span class="big-text">₽</span><br />
-                    <span class="small-text">максимальная сумма</span>
-                </div>
-                <div class="col-12 col-xl-4">
-                    от <span class="big-text">9.9%</span><br />
-                    <span class="small-text">ставка на весь срок</span>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-6 more-dierails border-top">
-                    <a href="#">Подробнее</a>
+                <div class="Advantagesstyled__CardsContainer-sc-67zj0h-2 ckfICs">
+                    <article class="Advantagesstyled__Body-sc-67zj0h-6 kuloBk"><p
+                                class="Advantagesstyled__Counter-sc-67zj0h-7 iBjoSA">1.</p><h4
+                                class="Advantagesstyled__Title-sc-67zj0h-8 dBbEdk">Заполните заявку</h4>
+                        <p class="Advantagesstyled__Text-sc-67zj0h-9 llVOKL"><a
+                                    class="Advantagesstyled__ColoredText-sc-67zj0h-10 ehMdYE">Онлайн</a> или в <a
+                                    class="Advantagesstyled__ColoredText-sc-67zj0h-10 ehMdYE">офисе</a> банка</p></article>
+                    <article class="Advantagesstyled__Body-sc-67zj0h-6 kozmcS"><p
+                                class="Advantagesstyled__Counter-sc-67zj0h-7 iBjoSA">2.</p><h4
+                                class="Advantagesstyled__Title-sc-67zj0h-8 dBbEdk">Дождитесь решения</h4>
+                        <p class="Advantagesstyled__Text-sc-67zj0h-9 llVOKL">Это займет от 2 минут</p></article>
+                    <article class="Advantagesstyled__Body-sc-67zj0h-6 jsvCKf"><p
+                                class="Advantagesstyled__Counter-sc-67zj0h-7 iBjoSA">3.</p><h4
+                                class="Advantagesstyled__Title-sc-67zj0h-8 dBbEdk">Получите деньги</h4>
+                        <p class="Advantagesstyled__Text-sc-67zj0h-9 llVOKL">Приходите в <a
+                                    class="Advantagesstyled__ColoredText-sc-67zj0h-10 ehMdYE">офис</a> Банка с документами и
+                            забирайте деньги</p></article>
                 </div>
             </div>
         </div>
     </div>
-    <div class="col-12 col-lg-6 block-item item-mch">
-        <div class="item-card">
-            <div class="block-img"></div>
-            <div class="row">
-                <div class="col-6">
-                    <h4>Кредит для пpедпpинимателей</h4>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-xl-4">
-                    до <span class="big-text">250</span> тыс. <span class="big-text">₽</span><br />
-                    <span class="small-text">максимальная сумма</span>
-                </div>
-                <div class="col-12 col-xl-4">
-                    от <span class="big-text">9.9%</span><br />
-                    <span class="small-text">ставка на весь срок</span>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-6 more-dierails border-top">
-                    <a href="#">Подробнее</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-12 col-lg-6 block-item item-mch">
-        <div class="item-card">
-            <div class="block-img"></div>
-            <div class="row">
-                <div class="col-6">
-                    <h4>Кредит для пpедпpинимателей</h4>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-xl-4">
-                    до <span class="big-text">250</span> тыс. <span class="big-text">₽</span><br />
-                    <span class="small-text">максимальная сумма</span>
-                </div>
-                <div class="col-12 col-xl-4">
-                    от <span class="big-text">9.9%</span><br />
-                    <span class="small-text">ставка на весь срок</span>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-6 more-dierails border-top">
-                    <a href="#">Подробнее</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
