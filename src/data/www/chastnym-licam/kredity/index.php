73eb6e8b6038b59b33d8ce717f5344faa1e61ca2
merinovroman <merinovroman@gmail.com>
2020-04-14 23:16:13 +0500

    раздел Кредиты 4 блока предложений

--- src/data/www/chastnym-licam/kredity/index.php
+++ src/data/www/chastnym-licam/kredity/index.php
@@ -31,8 +31,109 @@ $APPLICATION->SetTitle("Кредиты");
                         </div>
                     </div>
                 </div>
-                <div class="col-12 col-md-6">
+                <div class="col-12 col-md-6"></div>
+            </div>
+        </div>
+    </div>
+</div>
 
+<div class="row cards-deck">
+    <div class="col-12 col-lg-6 block-item item-mch">
+        <div class="item-card">
+            <div class="block-img"></div>
+            <div class="row">
+                <div class="col-6">
+                    <h4>Кредит для пpедпpинимателей</h4>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-xl-4">
+                    до <span class="big-text">250</span> тыс. <span class="big-text">₽</span><br />
+                    <span class="small-text">максимальная сумма</span>
+                </div>
+                <div class="col-12 col-xl-4">
+                    от <span class="big-text">9.9%</span><br />
+                    <span class="small-text">ставка на весь срок</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 more-dierails border-top">
+                    <a href="#">Подробнее</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-lg-6 block-item item-mch">
+        <div class="item-card">
+            <div class="block-img"></div>
+            <div class="row">
+                <div class="col-6">
+                    <h4>Кредит для пpедпpинимателей</h4>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-xl-4">
+                    до <span class="big-text">250</span> тыс. <span class="big-text">₽</span><br />
+                    <span class="small-text">максимальная сумма</span>
+                </div>
+                <div class="col-12 col-xl-4">
+                    от <span class="big-text">9.9%</span><br />
+                    <span class="small-text">ставка на весь срок</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 more-dierails border-top">
+                    <a href="#">Подробнее</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-lg-6 block-item item-mch">
+        <div class="item-card">
+            <div class="block-img"></div>
+            <div class="row">
+                <div class="col-6">
+                    <h4>Кредит для пpедпpинимателей</h4>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-xl-4">
+                    до <span class="big-text">250</span> тыс. <span class="big-text">₽</span><br />
+                    <span class="small-text">максимальная сумма</span>
+                </div>
+                <div class="col-12 col-xl-4">
+                    от <span class="big-text">9.9%</span><br />
+                    <span class="small-text">ставка на весь срок</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 more-dierails border-top">
+                    <a href="#">Подробнее</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-lg-6 block-item item-mch">
+        <div class="item-card">
+            <div class="block-img"></div>
+            <div class="row">
+                <div class="col-6">
+                    <h4>Кредит для пpедпpинимателей</h4>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-xl-4">
+                    до <span class="big-text">250</span> тыс. <span class="big-text">₽</span><br />
+                    <span class="small-text">максимальная сумма</span>
+                </div>
+                <div class="col-12 col-xl-4">
+                    от <span class="big-text">9.9%</span><br />
+                    <span class="small-text">ставка на весь срок</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 more-dierails border-top">
+                    <a href="#">Подробнее</a>
                 </div>
             </div>
         </div>
